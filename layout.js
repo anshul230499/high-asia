8d2d0297d2f76adfc172ecd71bb3ff414ec3df70
anshul230499 <anshul230499@gmail.com>
2026-08-25 21:25:17 -0500

    Delete layout.js

--- layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css'
-export const metadata={title:'High Asia',description:'Mountains, journeys and life at altitude.'}
-export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
